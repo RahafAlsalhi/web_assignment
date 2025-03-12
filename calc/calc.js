5aeaee638bda8399159fb14ed50944272c40fdb4
RahafAlsalhi <rahaf.alsalhi@gmail.com>
2025-03-12 23:57:53 +0300

    commit

---
+++ calc/calc.js
@@ -0,0 +1,26 @@
+function calculate() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let operation = document.getElementById('operation').value;
+
+    let result;
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            result = num2 !== 0? num1 / num2 : 'Cannot divide by zero';
+            break;
+        default:
+            result = 'Invalid operation';
+       
+    }
+
+ document.getElementById('result').value=result ;
+}
