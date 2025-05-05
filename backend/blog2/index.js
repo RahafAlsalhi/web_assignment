b12460736dc51dc36ec71425322cb723a03ef140
RahafAlsalhi <rahaf.alsalhi@gmail.com>
2025-05-06 01:33:00 +0300

    commit

---
+++ backend/blog2/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
